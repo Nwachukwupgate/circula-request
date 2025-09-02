dd71ecf1eb81edc2e7a47045935835eed5e0027a
treyp99 <nwachukwupgate99@gmail.com>
2025-09-02 10:37:17 +0100

    update users after login

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { SaveUser } from "api/userSlice";
+import { apiSlice } from "api/apiSlice";
 
 export default function Logout() {
   const navigate = useNavigate();
@@ -11,9 +12,10 @@ export default function Logout() {
   useEffect(() => {
     // Clear token, user data, etc.
     localStorage.clear();
-    dispatch(SaveUser(null)); // Clear user from redux store
+    dispatch(SaveUser(null)); 
+    dispatch(apiSlice.util.resetApiState());
     navigate("/authentication/sign-in");
   }, [dispatch, navigate]);
 
-  return null; // You can also return a spinner or blank screen
+  return null;
 }
